de2dc0173da1e727b7bbf3d6f41ec67fddeec331
Fabio <fabiocaimano@hotmail.it>
2021-06-18 23:20:40 +0200

    updated password validator project

---
+++ unit-tests/PasswordValidatorLibrary/PasswordValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PasswordValidatorLibrary
+{
+    public class PasswordValidator
+    {
+        public bool IsValid(string password)
+        {
+            /*
+             * The password length must be at least eight characters and a maximum of twenty characters
+             * The password must contain one or more uppercase characters
+             * The password must contain one or more lowercase characters
+             * The password must contain one or more numeric values
+             * The password must contain one or more special characters in the list @#!$%
+             */
+            Regex passwordPolicyExpression = new Regex(@"((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!$%]).{8,20})");
+            return passwordPolicyExpression.IsMatch(password);
+        }
+    }
+}
---
+++ unit-tests/PasswordValidatro_Tests/PasswordValidatorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using PasswordValidatorLibrary;
+
+
+
+namespace PasswordValidatro_Tests
+{
+    public class PasswordValidatorTest
+    {
+        [Fact]
+        public void PasswordValidateShouldBeTrue()
+        {
+            //Arrange
+            PasswordValidator password = new PasswordValidator();
+
+            bool expected = true;
+
+            // Act
+            bool actual = password.IsValid("P0veryV01!");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(true, "P@ram4T32")]
+        [InlineData(false, "Pallino")]
+
+        public void ValidPassword(bool expected, string password)
+        {
+            //Arrange
+            PasswordValidator passwordValidation = new PasswordValidator();
+
+            
+
+            // Act
+            bool actual = passwordValidation.IsValid(password);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
